c143c382e53ff02b4c8b9322caf3b1c58fcec1c9
i.sakharov <i731901@gmail.com>
2024-09-03 21:45:13 +0300

    everything was changed 11

--- bin/gendif.js
+++ bin/gendif.js
@@ -1,17 +1,20 @@
 #!/usr/bin/env node
+
 import { Command } from "commander";
 import makeCheck from "../src/index.js";
+
 const program = new Command();
 
 program
+  .name("gendiff")
   .version("0.1.0")
   .description("Compares two configuration files and shows a difference.")
-  .argument("<filepath1>")
-  .argument("<filepath2>")
+  .arguments("<filepath1> <filepath2>")
   .option("-f, --format <type>", 'output format (default: "stylish"')
   .helpOption("-h, --help", "output usage information")
   .action((filepath1, filepath2, option) => {
-    console.log(makeCheck(filepath1, filepath2, option.format));
+    const diff = makeCheck(filepath1, filepath2, option.format);
+    console.log(diff);
   });
 
 program.parse();
