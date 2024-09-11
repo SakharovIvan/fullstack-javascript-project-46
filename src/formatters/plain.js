75ff0e5ede072ccd5cc6900b8aeda2476cd47cbf
i.sakharov <i731901@gmail.com>
2024-09-11 22:15:11 +0300

    1109-6

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -14,8 +14,8 @@ const makePlain = (tree) => {
     const result = value.map((str) => {
       const
         {
-           key, action, oldValue, children, newValue,
-        } 
+         key, action, oldValue, children, newValue,
+        }
         =
         str;
       const path = momKey === '' ? `${key}` : `${momKey}.${key}`;
